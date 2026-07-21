f175d41a544c1a189202b4b69fee43ab6b45c70f
Tahta D. Timur <tahta@impactscope.com>
2026-07-21 20:53:10 +0700

    feat(web): added web layer and templui dependency

--- gen.go
+++ gen.go
@@ -1,6 +1,7 @@
 //go:generate go tool moq -out internal/mockrepo/document.go -pkg mockrepo internal/data DocumentRepoer
 //go:generate go tool moq -out internal/mockrepo/document_chunk.go -pkg mockrepo internal/data DocumentChunkRepoer
 //go:generate go tool moq -out internal/mockrepo/atomic_knowledge.go -pkg mockrepo internal/data AtomicKnowledgeRepoer
+//go:generate go tool moq -out internal/mockrepo/user.go -pkg mockrepo internal/data UserRepoer
 //go:generate go tool sqlc generate
 
 package main
